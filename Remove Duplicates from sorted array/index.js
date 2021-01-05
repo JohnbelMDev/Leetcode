9d85dbbb7e1576d037ec137dead13f28a7ba30ae
johnbelmdev <johnbel458@gmail.com>
2021-01-05 00:29:50 -0500

    new question

---
+++ Remove Duplicates from sorted array/index.js
@@ -0,0 +1,15 @@
+function test (nums){
+let temp
+for (var i = 0; i < nums.length; i++) {
+  nums[i]
+  // console.log(nums[i]);
+  if(!nums[i] ) {
+    console.log(nums[i]);
+  }
+}
+
+
+}
+
+
+console.log(test([1,2,2,3,4,5]));
