cbc10f1758a5e7b39b743105dd429f989ad68d3c
johnbelmdev <johnbel458@gmail.com>
2021-02-02 22:52:52 -0500

    commit

--- Reverse string/index.js
+++ Reverse string/index.js
@@ -1,21 +1,6 @@
 function reverseString (x){
-let num =2147483647;
-let num2 = -2147483647
-
-let temp = x.toString().split('').reverse().join('')
-   temp = parseInt(temp) * Math.sign(x);
-
-   if( x > num || x<num2){
-     return 0
-   }
-   else if (temp > num || temp < num2) {
- return 0
-   }
-   else{
-     return temp;
-   }
-
+  return x.reverse()
 
 }
 
-console.log(test(1534236469));
+console.log(reverseString(["h","e","l","l","o"]));
