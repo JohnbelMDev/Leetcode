0a68de65bcd5f3646865b3fa2117e2edc478be5d
johnbelmdev <johnbel458@gmail.com>
2021-02-09 23:08:38 -0500

    commit

---
+++ Longest Common Prefix/index.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+
+// take an array
+// the value of the array will be more than one
+// returning the longest prefix in the array
+
+var longestCommonPrefix = function(strs) {
+
+};
