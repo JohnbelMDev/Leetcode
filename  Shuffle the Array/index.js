07879f283bb40e8a854545190db06acbe6f0e815
johnbelmdev <johnbel458@gmail.com>
2021-02-19 22:13:03 -0500

    commit

---
+++  Shuffle the Array/index.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+
+//  Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+//
+// Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+var shuffle = function(nums, n) {
+
+};
