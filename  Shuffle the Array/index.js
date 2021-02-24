f8b0b3a1e13ed0cc91b2862a0b0378db7f40b444
johnbelmdev <johnbel458@gmail.com>
2021-02-24 17:10:37 -0500

    commit

---  Shuffle the Array/index.js
+++  Shuffle the Array/index.js
@@ -8,5 +8,19 @@
 //
 // Return the array in the form [x1,y1,x2,y2,...,xn,yn].
 var shuffle = function(nums, n) {
+const half = nums.length / 2;
+let answer = []
+const firstHalf = nums.splice(0, half)
+const secondHalf = nums.splice(0,half)
+   for (let i = 0; i < firstHalf.length; i++) {
+     for (let z = 0; z < secondHalf.length; z++) {
+       if (i === z) {
+           answer.push(firstHalf[i],secondHalf[z])
+       }
+     }
+   }
+   return answer
 
 };
+
+console.log(shuffle([2,5,1,3,4,7]));
