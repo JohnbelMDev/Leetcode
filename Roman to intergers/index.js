6a922544ab966d5e605f95391d5872f898d89b72
johnbelmdev <johnbel458@gmail.com>
2021-06-17 22:37:18 -0400

    roman numerals

--- Roman to intergers/index.js
+++ Roman to intergers/index.js
@@ -1,40 +1,33 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ // Symbols for roman numerals and their number
+// Symbol       Value
+// I             1
+// V             5
+// X             10
+// L             50
+// C             100
+// D             500
+// M             1000
+ // params : Taking a roman numeral
+ // return value: Return as an integer
+ // example
+      //  I => 1
+      // III => 3
+      // "IV" => 4
+
+  // create an object to store the value of the Roman Numeral
+  //Romanobject = {
+          // I:1,
+          // V:5,
+          // X:10,
+          // L:50,
+          // C:100,
+          // M: 100
+    //}
+
 var romanToInt = function(s) {
-  let romanObject = {
-    "I": 1,
-    "V": 5,
-    "X": 10,
-    "L": 50,
-    "C": 100,
-    "D": 500,
-    "M": 1000
-
-  }
-  for (const property in romanObject) {
-    // console.log(property,romanObject[property]);
-    if (property === "I") {
-      console.log(romanObject[property]);
-    } else if (property === 'V') {
-      console.log(romanObject[property]);
-
-    } else if (property === 'X') {
-      console.log(romanObject[property]);
-
-    } else if (property === 'L') {
-      console.log(romanObject[property]);
-
-    } else if (property === 'C') {
-      console.log(romanObject[property]);
-
-    } else if (property === 'D') {
-      console.log(romanObject[property]);
-
-    } else if (property === 'M') {
-      console.log(romanObject[property]);
-
-    }
-
-  }
 
 };
-
-console.log(romanToInt());
